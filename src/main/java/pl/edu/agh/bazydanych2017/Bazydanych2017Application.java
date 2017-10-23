7861e81501b82bbb093c28866a4b798aa1002aae
Piotr Szewczyk <piotrszewczyk86@o2.pl>
2017-10-23 18:32:24 +0200

    [Issue #1] created initial spring boot setup

---
+++ src/main/java/pl/edu/agh/bazydanych2017/Bazydanych2017Application.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.bazydanych2017;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bazydanych2017Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bazydanych2017Application.class, args);
+	}
+}
